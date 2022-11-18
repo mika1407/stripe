3ac66ad148af9ef6f3357a9e205372b0cc123405
mika1407 <koneautomaatio@hotmail.com>
2022-11-18 07:33:24 +0200

    product card component

---
+++ src/components/ProductCard.js
@@ -0,0 +1,18 @@
+import {Cart, Button, Form, Row, Col, Card} from 'react-bootstrap';
+
+function ProductCard(props) { // props.product is the product we are selling
+    const product = props.product;
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>€{product.price}</Card.Text>
+                <Button variant="primary">Add To Cart</Button>
+            </Card.Body>
+        </Card>
+    )
+
+}
+
+export default ProductCard;
